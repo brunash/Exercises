9a565705ca16f1be9437d5958543706d4b008599
Slobodan Zaja <bobazso@yahoo.com>
2022-05-23 13:58:09 -0400

    first commit

---
+++ SumOfDifferencesInArray.js
@@ -0,0 +1,12 @@
+function sumOfDifferences(arr) {
+    return arr
+    .sort((a, b) => b - a)
+    .reduce((acc, curr, index, array) => {
+      const next = array[index + 1];
+      if (!isNaN(curr - next)) {
+        acc += curr - next;
+      }
+      return acc;
+    }, 0);
+}
+ 
